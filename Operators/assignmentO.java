664b6bbcc7fab43405275bcc61d1791a31ebefec
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-18 19:59:07 +0530

    New

---
+++ Operators/assignmentO.java
@@ -0,0 +1,28 @@
+public class assignmentO {
+    public static void main(String[] args) {
+        int A = 10;
+        //A = A + 10;
+        //A += 10;
+        //System.out.println(A); 5;
+
+        //int B = 5;
+        //B = B - 5;
+        // B -= 5;
+        // System.out.println(B);
+
+        // int B = 5;
+        // //B = B * 5;
+        // B *= 5;
+        // System.out.println(B);
+
+        // int B = 5;
+        // //B = B / 5;
+        // B /= 5;
+        // System.out.println(B);
+
+        int B = 5;
+        B = B % 5;
+        //B %= 5;
+        System.out.println(B);
+    }
+}
