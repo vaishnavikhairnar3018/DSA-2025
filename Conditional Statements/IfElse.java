a2b53458807845c55772fefee357b2b19f9cb734
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-19 11:11:16 +0530

    new

---
+++ Conditional Statements/IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 14;
+        if (age >= 18) {
+            System.out.println("You can vote");
+            }
+
+        if(age > 13 && age < 18) {
+            System.out.println("teenager");
+        }
+        else {
+            System.out.println("You can't vote");
+        }
+    }
+}
