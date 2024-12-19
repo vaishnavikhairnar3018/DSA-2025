5db1370c024a206083a47ba26333ca168b5e0ed8
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-19 15:49:14 +0530

    new

---
+++ Conditional Statements/SwitchStatements.java
@@ -0,0 +1,15 @@
+public class SwitchStatements {
+    public static void main(String[] args) {
+        int num = 2;
+
+        switch (num){
+            case 1 : System.out.println("samosa");
+            break;
+            case 2 : System.out.println("burger");
+            break;
+            case 3 : System.out.println("lassi");
+            break;
+            default : System.out.println("we realize we are dreaming");
+        }
+    }
+}
