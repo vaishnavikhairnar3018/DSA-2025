23b07dc4eb5cab27ff161b0d174fd823c0d0b724
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-19 15:03:23 +0530

    new

---
+++ Conditional Statements/TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int num = 4;
+
+        //ternary operator
+        String type =((num % 2) == 0) ? "even" : "odd";
+        System.out.println(type);
+}
+}
