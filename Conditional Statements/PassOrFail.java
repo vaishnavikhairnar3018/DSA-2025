9ca03e1e9624188ccbfa905639c0ccb984b633de
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-19 15:10:21 +0530

    new

---
+++ Conditional Statements/PassOrFail.java
@@ -0,0 +1,8 @@
+public class PassOrFail {
+    public static void main(String[] args) {
+        int marks = 88;
+
+        String ReportCard = marks >= 33 ? "Pass" : "Fail";
+        System.out.println(ReportCard); 
+    }
+}
