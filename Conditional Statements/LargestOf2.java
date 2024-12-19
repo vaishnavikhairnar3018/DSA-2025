d25f79a3bec865c7a6aac3fd5856e058cffb0025
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-19 11:33:50 +0530

    Added new files

---
+++ Conditional Statements/LargestOf2.java
@@ -0,0 +1,14 @@
+public class LargestOf2 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 78;
+
+        if (A > B) {
+            System.out.println("A is greater than B");
+        }
+
+        else {
+            System.out.println("B is greater than A");
+        }
+    }
+}
