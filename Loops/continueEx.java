60866ebd0da22fb858eb22c26c38df67d945a99f
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-21 23:11:48 +0530

    new

---
+++ Loops/continueEx.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class continueEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Enter a munber :");
+            int n = sc.nextInt();
+
+            if(n % 10 == 0) {
+                continue;
+            }
+            System.out.println("number was : " + n);
+        }while(true);
+}
+}
