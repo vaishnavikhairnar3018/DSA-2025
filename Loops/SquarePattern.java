717fc02bb6577a9cc025029a104d889a929c4e22
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-21 19:22:27 +0530

    new

---
+++ Loops/SquarePattern.java
@@ -0,0 +1,8 @@
+public class SquarePattern {
+    public static void main(String[] args) {
+        
+        for (int lines = 1; lines <= 4; lines++) {
+            System.out.println("****");
+        }
+    }
+}
