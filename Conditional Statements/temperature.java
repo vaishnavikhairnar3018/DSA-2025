71a4d21a5228dd4dc71269acac757c96a0c3c205
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-21 09:44:10 +0530

    new

---
+++ Conditional Statements/temperature.java
@@ -0,0 +1,12 @@
+public class temperature {
+    public static void main(String[] args) {
+        double temp = 104.5;
+
+        if (temp > 100) {
+            System.out.println("You have fewer");
+        }
+        else {
+            System.out.println("You don't have fewer.");
+        }
+    }
+}
