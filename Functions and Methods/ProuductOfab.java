d112620a4c79082bbdd768a4b4df3c13225f20e1
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-24 23:27:00 +0530

    new

---
+++ Functions and Methods/ProuductOfab.java
@@ -0,0 +1,15 @@
+
+public class ProuductOfab {
+    
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+    
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 2;
+        int prod = multiply(a, b);
+        System.out.println(prod);
+    }
+}
