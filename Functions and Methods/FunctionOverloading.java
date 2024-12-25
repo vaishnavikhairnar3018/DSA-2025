4152ff03d157cb5e6d8e534702a6b68a3c068a8a
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-25 09:45:18 +0530

    new

---
+++ Functions and Methods/FunctionOverloading.java
@@ -0,0 +1,16 @@
+
+public class FunctionOverloading {
+    
+public static int sum (int a, int b) {
+    return a + b;
+}  
+
+public static int sum (int a, int b, int c) {
+    return a + b + c;
+} 
+    
+    public static void main(String[] args) {
+        System.out.println(sum(3, 5));
+        System.out.println(sum(5, 2, 1));
+    }
+}
