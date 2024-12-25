266b3935aaf4ac0b549412dfda7d0b48370899b3
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-25 11:02:58 +0530

    new

---
+++ Functions and Methods/PrimeOrNot3.java
@@ -0,0 +1,30 @@
+
+public class PrimeOrNot3 {
+    
+    
+    public static boolean isPrime(int n) {
+        if (n == 2){
+            return true;
+        }
+    
+            for(int i = 2; i<=Math.sqrt(n); i++){
+                if (n % i == 0){
+                    return false;
+                }
+            }
+            return true;
+       }
+
+       public static void primesInRange(int n){
+            for(int i = 2; i<=n; i++){
+                if(isPrime(i)) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println();
+       }
+    
+       public static void main(String[] args) {
+        primesInRange(20);
+    }
+}
