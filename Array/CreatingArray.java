8ea85c6de9a1b30f5d0fbd393e7546c82a1264ae
Vaishnavi Khairnar <vaishnavikhairnar799@gmail.com>
2024-12-26 19:21:05 +0530

    new folder

---
+++ Array/CreatingArray.java
@@ -0,0 +1,12 @@
+public class CreatingArray {
+    public static void main(String[] args) {
+        int marks[] = new int[50]; // Array of integers with 50 elements
+
+        int numbers[] = {1, 2, 3}; // Array initialized with values
+
+        int moreNumbers[] = {4, 5, 6}; // Another array with values
+
+        String fruits[] = {"apple", "mango", "orange"}; // Array of strings
+    }
+}
+
